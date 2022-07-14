5b17817b3389ef4ec9156173da0aeb1a352c6eb7
RitvarsZ <ritvarszvejnieks@protonmail.com>
2022-07-14 11:36:40 +0300

    Add image cards to homepage.

---
+++ app/src/components/Card.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="card">
+    <img :src="imgSrc" :alt="description" class="card-image" />
+    <h2 class="card-title">{{ title }}</h2>
+    <p class="card-desc">{{ description }}</p>
+    <div>
+      <router-link :to="{ name: 'Details', params: { id: id } }">
+        <button class="button-link">Details</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+  max-width: 300px;
+}
+
+.card:hover {
+  position: relative;
+  overflow: auto;
+}
+
+.card:hover .card-desc {
+  opacity: 1;
+  display: block;
+}
+
+.card-image {
+  aspect-ratio: 16/9;
+  object-fit: cover;
+  width: fill-available;
+  margin-bottom: 1rem;
+}
+
+.card-title {
+  font-size: 1.2rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+.card-desc {
+  font-size: 0.8rem;
+  margin: 0 1rem 0.5rem 1rem;
+  display: none;
+}
+
+.button-link {
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 0.25rem;
+  color: #000;
+  font-size: 0.8rem;
+  padding: 0.5rem;
+  text-decoration: none;
+  text-transform: uppercase;
+  margin-bottom: 1rem;
+}
+
+.button-link:hover {
+  cursor: pointer;
+  background-color: rgb(12, 133, 255);
+  color: #fff;
+  transition: 150ms ease-in-out;
+}
+</style>
--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>
-    List of images
+  <div class="card-grid">
+    <Card v-for="image in images" :key="image.id"
+      :imgSrc="image.coco_url"
+      :title="image.file_name"
+      :description="getObjectString(image.objects)"
+      :id="image.id"/>
   </div>
 </template>
 
 <script>
-import { getAllImages } from '../data/dataHelper';
+import { getAllImages } from '@/data/dataHelper';
+import Card from '@/components/Card.vue';
 
 export default {
+  components: {
+    Card,
+  },
   data() {
     return {
       images: [],
@@ -16,5 +24,31 @@ export default {
   created() {
     this.images = getAllImages();
   },
+  methods: {
+    getObjectString(objects) {
+      let string = '';
+      objects.forEach((object) => { string += `${object.name}, `; });
+      string = string.charAt(0).toUpperCase() + string.slice(1);
+      string = string.slice(0, -2); // remove last comma
+
+      return string;
+    },
+  },
 };
 </script>
+
+<style scoped>
+.card-grid {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.1);
+  gap: 1rem;
+  padding: 1rem;
+}
+</style>
